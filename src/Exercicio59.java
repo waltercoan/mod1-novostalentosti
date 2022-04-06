d1e56dd82f9e7d1635159154ae93776c55b3ebca
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-04-06 15:07:13 -0300

    exercicio 59

--- src/Exercicio59.java
+++ src/Exercicio59.java
@@ -5,13 +5,26 @@
  10 graus Celsius e finalizando em 100 graus Celsius. 
  O programa deverá apresentar os valores das duas 
  temperaturas.
+ENTRADA
+NAO TEM!!!
 
- PROCESSAMENTO
- F = 
+PROCESSAMENTO
+- Gerar temperaturas em celsius de 10 ate 100 (passo/step 10)
+F = (c * 1.8) + 32
+
+SAIDA
+- Temp em Celsius e Fahrenheit
 */
 
 public class Exercicio59 {
     public static void main(String[] args) {
-        
+        //tempc += 10
+        //tempc = tempc + 10
+        for(int tempc = 10; tempc <= 100; tempc += 10){
+            float tempf = (tempc * 1.8f) + 32f;
+            System.out.println(
+                String.format("Celsius %d - Fahrenheit %.2f", tempc, tempf));
+            //System.out.println("Celsius " + tempc + " Fahrenheit " + tempf);
+        }
     }
 }
